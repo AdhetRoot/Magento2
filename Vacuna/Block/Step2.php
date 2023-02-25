7a5e490fe09c7439f507ef0a36bcb8a95ef8fbb8
AdhetRoot <kaliadhet@gmail.com>
2023-02-25 00:58:45 +0000

    AT007

---
+++ Vacuna/Block/Step2.php
@@ -0,0 +1,16 @@
+<?php
+namespace Farmacia\Vacuna\Block;
+class Step2 extends \Magento\Framework\View\Element\Template
+{
+	protected $_postFactory;
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context
+	)
+	{
+		parent::__construct($context);
+	}
+
+	
+       
+
+}
--- Vacuna/Controller/Index/Step1.php
+++ Vacuna/Controller/Index/Step1.php
@@ -1,27 +1,42 @@
 <?php
 namespace Farmacia\Vacuna\Controller\Index;
-use Magento\Customer\Model\Session;
-class Step1 extends \Magento\Framework\App\Action\Action
-{
-	protected $_pageFactory;
-	protected $_customerSession;
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context, Session $customerSession,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
-	{
-		$this->_customerSession = $customerSession;
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
 
-	public function execute()
-	{
-		if (!$this->_customerSession->isLoggedIn()) {
-			$this->_redirect('customer/account/login');
-			return;
-		}
-		$resultPage = $this->$pageFactory->create();
-		$resultPage->getConfig()->getTitle()->prepend(__('Step 1'));
-		return $resultPage;
-	}
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Step1 extends Action
+{
+    protected $pageFactory;
+    protected $customerSession;
+    protected $sessionManager;
+
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory,
+        Session $customerSession,
+        SessionManagerInterface $sessionManager
+    ) {
+        $this->pageFactory = $pageFactory;
+        $this->customerSession = $customerSession;
+        $this->sessionManager = $sessionManager;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        if (!$this->customerSession->isLoggedIn()) {
+            $this->_redirect('customer/account/login');
+            return;
+        }
+
+        $name = "HOLA " . $this->customerSession->getCustomer()->getName();
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__($name));
+        return $resultPage;
+    }
+
+ 
 }
